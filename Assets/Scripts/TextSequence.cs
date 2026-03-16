fe76f98c3054770d49ffd085402af10bb9e4bb5b
munsung143 <akunta3475@gmail.com>
2026-03-16 20:46:34 +0900

    [fix] 빌드오류 수정

--- Assets/Scripts/TextSequence.cs
+++ Assets/Scripts/TextSequence.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using TMPro;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
