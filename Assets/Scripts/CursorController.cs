234543d60f7c310a48e879af997498a2b3929ab9
munsung143 <akunta3475@gmail.com>
2026-04-02 21:50:16 +0900

    [feat] 화면 회전 기능 추가

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.iOS;
 using UnityEngine.UI;
 
 public interface ICursorEffectProvider
@@ -20,29 +21,45 @@ public class CursorController : MonoBehaviour, ICursorEffectProvider
     bool screenOn;
 
     bool reverse;
+
+    int screenWidth;
+    int screenHeight;
+    float ratio;
     void Awake()
     {
         cam = Camera.main;
         AskingEventRegistry.cursor = this;
+        screenWidth = UnityEngine.Device.Screen.width;
+        screenHeight = UnityEngine.Device.Screen.height;
+        ratio = (float)screenHeight/screenWidth;
     }
     void Update()
     {
         if (screen == null) return;
         Vector3 input = Input.mousePosition;
         input.z = cameraDepth;
-        Vector3 i = screen.InverseTransformPoint(cam.ScreenToWorldPoint(input));
+        Vector3 s2w = cam.ScreenToWorldPoint(input);
+        Vector3 i = screen.InverseTransformPoint(s2w);
         if (i.x > halfWidth || i.x < -halfWidth || i.y > halfHelght || i.y < -halfHelght || !screenOn)
         {
             Cursor.visible = true;
-            return;
+            float yaw = (input.x / screenWidth - 0.5f) * 20 * ratio;
+            float pitch = (input.y / screenHeight - 0.5f) * -20 * ratio;
+            //cam.transform.rotation = Quaternion.Euler(new Vector3(pitch, yaw, 0));
+            cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, Quaternion.Euler(new Vector3(pitch, yaw, 0)), 0.1f);
         }
-        Cursor.visible = false;
-        if (reverse)
+        else
         {
-            i.x = -i.x;
-            i.y = -i.y;
+            cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, Quaternion.identity, 0.1f);
+            Cursor.visible = false;
+            if (reverse)
+            {
+                i.x = -i.x;
+                i.y = -i.y;
+            }
+            i.z = 0;
+            transform.localPosition = i;
         }
-        transform.localPosition = i;
 
     }
     public void SetValues(RectTransform trs, float depth)
@@ -67,7 +84,7 @@ public class CursorController : MonoBehaviour, ICursorEffectProvider
     }
     public void Reset()
     {
-        reverse =  false;
+        reverse = false;
         image.color = Color.white;
     }
 }
